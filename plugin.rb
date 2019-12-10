ca805e05a3e60922dab731fb0654f9157ef970c9
iviguera <ivanviguera@gmail.com>
2019-12-10 20:15:55 +0100

    Preparando la clase a interceptar

--- plugin.rb
+++ plugin.rb
@@ -1,1 +1,13 @@
+# frozen_string_literal: true
 
+module EditEveEmailChangeTo
+  def change_to(email_input)
+    ## stuff
+    super(email_input)
+  end
+end
+
+require_dependency 'email_updater'
+class ::EmailUpdater
+  singleton_class.prepend EditEveEmailChangeTo
+end
