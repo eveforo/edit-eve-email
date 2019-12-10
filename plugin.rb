ea4794018de2dc8153fcaf104f4cf2b206c77dda
iviguera <ivanviguera@gmail.com>
2019-12-10 22:37:12 +0100

    confirmando directamente el cambio de mail

--- plugin.rb
+++ plugin.rb
@@ -1,13 +1,56 @@
 # frozen_string_literal: true
 
-module EditEveEmailChangeTo
+# name: edit-eve-email
+# about: Doesnt send confirmation email.
+# version: 0.0.1
+# authors: Hikryon - Iván Viguera.
+
+register_asset "javascripts/discourse/templates/connectors/edit-eve-email-links/edit-eve-email-links.hbs"
+
+module EditEveEmailChangeToExtension
   def change_to(email_input)
-    ## stuff
-    super(email_input)
+    @guardian.ensure_can_edit_email!(@user)
+
+    email = Email.downcase(email_input.strip)
+    EmailValidator.new(attributes: :email).validate_each(self, :email, email)
+
+    if (existing_user = User.find_by_email(email))
+      if SiteSetting.hide_email_address_taken
+        Jobs.enqueue(:critical_user_email, type: :account_exists, user_id: existing_user.id)
+      else
+        error_message = +'change_email.error'
+        error_message << '_staged' if existing_user.staged?
+        errors.add(:base, I18n.t(error_message))
+      end
+    end
+
+    if errors.blank? && existing_user.nil?
+      args = {
+          old_email: @user.email,
+          new_email: email,
+      }
+
+      if authorize_both?
+        args[:change_state] = EmailChangeRequest.states[:authorizing_old]
+        email_token = @user.email_tokens.create!(email: args[:old_email])
+        args[:old_email_token] = email_token
+      else
+        args[:change_state] = EmailChangeRequest.states[:authorizing_new]
+        email_token = @user.email_tokens.create!(email: args[:new_email])
+        args[:new_email_token] = email_token
+      end
+      @user.email_change_requests.create!(args)
+
+      Rails.logger.warn("EditEveEmailChangeToExtension token: #{email_token.token}")
+
+      confirm(email_token.token)
+    end
+
+      #super(email_input)
   end
 end
 
 require_dependency 'email_updater'
 class ::EmailUpdater
-  singleton_class.prepend EditEveEmailChangeTo
+  prepend EditEveEmailChangeToExtension
 end
